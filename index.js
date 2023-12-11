6ead86d0bff0e0f27226e0048dc22f415f35c677
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-11 22:58:23 +0530

    comit

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcrypt') //password encryption
-const jst = require('jsonwebtoken') //create session
+const jwt = require('jsonwebtoken') //create session
+const cookieParser = require('cookie-parser')
 const app = express();
 
 const salt= bcrypt.genSaltSync(10)//password encryption
+const secret = 'dh43bjfnsnjsn839nmncxr3' //used in jsw Token
 
-app.use(cors())
+app.use(cors({credentials:true, origin:'http://localhost:3000'})) //include additional info to cookie
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect('mongodb+srv://bullmoney0:Thisara@practice.fbucg2s.mongodb.net/?retryWrites=true&w=majority')
 
@@ -32,10 +35,30 @@ app.post('/login', async(req,res)=>{
     const passOk = bcrypt.compareSync(password, userDoc.password)
     if(passOk){
         //logged in
+        //create token
+        //        username   grab userID from userDoc assign to id
+        jwt.sign({username,id:userDoc.id}, secret, {} , (err,token)=>{
+            if(err) throw err
+            //send response as a cookie
+            res.cookie('token',token).json({
+                id:userDoc._id,
+                username
+            })
+        })
     }else{
         res.status(400).json('Wrong Credentials')
     }
 })
 
-
+app.get('/profile', (req,res)=>{
+    const {token} = req.cookies
+    jwt.verify(token, secret, {}, (err,info)=>{
+        if(err) throw err
+        res.json(info)
+    })
+    res.json(req.cookies) //WE CREATE TO DISABLE REGISTER AND LOGIN BUTTON WHEN LOGIN
+})
+app.post('/logout', (req,res)=>{
+    res.cookie('token','').json('ok')
+})
 app.listen(4000)
